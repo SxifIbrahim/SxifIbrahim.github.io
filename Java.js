93503927ace0ca2e4e61adb961539cead67a7fa3
SxifIbrahim <sibrahim.rtei@gmail.com>
2024-05-30 19:50:01 +0400

    First Push

---
+++ Java.js
@@ -0,0 +1,41 @@
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+
+let nav = document.querySelector('header');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    nav.style.top = value * 0.5 + 'px'
+})
+
+// VARIABLES
+const elH = document.querySelectorAll(".timeline li > div");
+
+// START
+window.addEventListener("load", init);
+
+function init() {
+  setEqualHeights(elH);
+}
+
+// SET EQUAL HEIGHTS
+function setEqualHeights(el) {
+  let counter = 0;
+  for (let i = 0; i < el.length; i++) {
+    const singleHeight = el[i].offsetHeight;
+
+    if (counter < singleHeight) {
+      counter = singleHeight;
+    }
+  }
+
+  for (let i = 0; i < el.length; i++) {
+    el[i].style.height = `${counter}px`;
+  }
+}
